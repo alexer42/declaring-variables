bb541b445c818475ceae7f1bcebd890a1534d508
alexer42 <alexandre.dziubate@escola.pr.gov.br>
2026-03-19 14:59:54 +0000

    salvar alterações

--- script.js
+++ script.js
@@ -26,12 +26,45 @@ area = altura * comprimento
 console.log(area);
 const forma = 'quadrado'
 */
+// Area Retangulo
+function retangulo() {
+    const altura_ = 5;
+    const comprimento = 7;
+    const area_retangulo = altura_ * comprimento;
+    return area_retangulo;
+}
 
-const altura = 5;
-const comprimento = 7;
-const area = altura * comprimento;
-console.log(area);
 
+// Area Triângulo
+function triangulo() {
+    const base = 5;
+    const altura_triangulo = 7;
+    const area_triangulo = base * altura_triangulo / 2;
+    return area_triangulo;
+}
+
+// Area Quadrado
+function quadrado() {
+    const lado = 5;
+    const area_quadrado = lado * lado;
+    return area_quadrado;
+}
+
+function mostra_area() {
+    const formas = ['retangulo', 'triangulo', 'quadrado', 'Forma desconhecida'];
+    const forma = formas[Math.floor(Math.random() * formas.length)];
+    if (forma === 'retangulo') {
+        console.log('Retângulo: ' + retangulo());
+    } else if (forma === 'triangulo') {
+        console.log('Triângulo: ' + triangulo());
+    } else if (forma === 'quadrado') {
+        console.log('Quadrado: ' + quadrado());
+    } else {
+        console.log('Forma desconhecida');
+    }
+}
+
+mostra_area();
 
 
 
