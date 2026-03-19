e7df89db74aa997081ea2d7229ed2aa6330ecab1
alexer42 <alexandre.dziubate@escola.pr.gov.br>
2026-03-19 11:20:11 -0300

    salvando

---
+++ script.js
@@ -0,0 +1,49 @@
+/* Teste Area 1
+
+var
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento
+conole.log(area);
+*/
+
+/*teste 2
+
+let forma = 'retangulo'
+let altura = 5;
+let comprimento = 7;
+let area;
+
+*/         
+
+/* teste 3
+if(forma === 'retangulo'){
+area = altura * comprimento
+}else {
+    area = ( altura * comprimento / 2)
+    }
+console.log(area);
+const forma = 'quadrado'
+*/
+
+const altura = 5;
+const comprimento = 7;
+const area = altura * comprimento;
+console.log(area);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
